da3db15ca6225affc6588907fbecd140459d2f26
Tsubasa785 <79652612+Tsubasa785@users.noreply.github.com>
2025-04-22 00:34:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -62,3 +62,7 @@ def handle_message(event):
         reply_text = "[Translation error] " + str(e)
 
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
